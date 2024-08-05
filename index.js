a4b73f6c90d035fb9a9ac9c1d59c4c9fc1f2ca82
gabizoca1209 <164278644+gabizoca1209@users.noreply.github.com>
2024-08-05 22:56:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const meuNome = "Gabriella Kaipper";
+const idade = 17;
+const altura = 1.65;
+const massa = "66 Kg";
+const cidade = "sjp"
+
+console.log(meuNome)
+console.log(idade)
+console.log(altura)
+console.log(massa)
+console.log(cidade)
+
+
+
+
+// adicione nesse programa, altura, massa e cidade onde mora
